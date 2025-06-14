59f436a8b5b52ed18e87161498c8642f22391ba3
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2025-06-14 14:19:28 +0530

    removed api key from github

---
+++ src/utils/constant.js
@@ -0,0 +1,11 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+export const USER_AVATAR = "https://avatars.githubusercontent.com/u/130219141?v=4"
+
+export const API_OPTION = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: import.meta.env.VITE_API_AUTH_TOKEN
+  }
+};
