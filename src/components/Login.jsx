c2d207cf80103f1ce6743a0343ea3756e3e42377
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2025-06-14 14:01:20 +0530

    created a netflix with signIn/signUp authentication and added a TMDP api

---
+++ src/components/Login.jsx
@@ -0,0 +1,156 @@
+import { useState, useRef } from "react";
+import Header from "./Header";
+import { validateData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { LOGO, USER_AVATAR } from "../utils/constant";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const dispatch = useDispatch();
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+  // useRef for referencing the input data from FORM...
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
+
+  // signIn/Up button click logic
+  const handleSubmit = () => {
+    // data validation using utils called validate.js
+    const message = validateData(email.current.value, password.current.value);
+    // console.log(message);
+    setErrorMessage(message); //error messaging displaying using a useState
+    if (message) return; //if the msg contains a string it means it's not valid so returning here.
+    if (!isSignInForm) {
+      //sign up form
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+        
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: USER_AVATAR,
+          })
+            .then(() => {
+              // Profile updated!
+              // ...
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              
+            })
+            .catch((error) => {
+              // An error occurred
+              // ...
+              setErrorMessage(error.message);
+            });
+          // navigate("/browse");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + " " + errorMessage);
+          // ..
+        });
+    } else {
+      //sign in form
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + " " + errorMessage);
+        });
+    }
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src={LOGO}
+          alt="logo"
+        />
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <p className="text-red-600 text-lg font-bold text-center py-5">
+          {errorMessage}
+        </p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleSubmit}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p>
+      </form>
+    </div>
+  );
+};
+export default Login;
---
+++ src/utils/constant.js
@@ -0,0 +1,11 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+export const USER_AVATAR = "https://avatars.githubusercontent.com/u/130219141?v=4"
+
+export const API_OPTION = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYjMzMmViZWEzNmViYmJlZDU1N2ZlZmMyMzlkODBkYyIsIm5iZiI6MTc0Njc5MzU1Ny4yMDcsInN1YiI6IjY4MWRmNDU1ZGQ3MGNhYzRkNzg4YzdlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NZZX6UCJquxMrmjkZ9pV8bOeEILF1mtWSXNCpgTgMSw'
+  }
+};
