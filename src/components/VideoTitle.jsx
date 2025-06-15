d5c29aaae821f1f79e06489865285c9c6fe77ddf
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2025-06-15 20:32:36 +0530

    added videoBackground video

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -6,10 +6,10 @@ const VideoTitle = ({ title, overview }) => {
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div className="">
-        <button className="bg-gray-300 text-white p-4 px-12 text-xl items-center rounded-lg">
+        <button className="bg-gray-300 text-white p-4 px-12 text-xl items-center rounded-lg cursor-pointer">
           ▶Play
         </button>
-        <button className="bg-gray-300 mx-2 text-white p-4 px-12 text-xl items-center rounded-lg">
+        <button className="bg-gray-300 mx-2 text-white p-4 px-12 text-xl items-center rounded-lg cursor-pointer">
           More Info
         </button>
       </div>
