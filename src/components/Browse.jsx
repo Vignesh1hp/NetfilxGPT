d057141c7f52fca39860dd9de82a7f04254d7470
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2025-06-15 14:25:19 +0530

    added videoTitle and fixed error in constant.js api

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,18 +1,16 @@
-import React, { useEffect } from 'react'
 import Header from './Header';
-import { API_OPTION } from '../utils/constant';
-
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
+import MainContainer from './MainContainer';
+import SecondaryContainer from './SecondaryContainer';
 const Browse = () => {
-  const getNowPlayingMovies= async()=>{
-    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTION);
-    const json = await data.json();
-    console.log(json);
-  }
-  useEffect(()=>{
-    getNowPlayingMovies();
-  },[])
+  useNowPlayingMovies();
   return (
-    <Header/>
+    <div>
+        <Header/>
+        <MainContainer/>
+        <SecondaryContainer/>
+    </div>
+    
   )
 }
 
